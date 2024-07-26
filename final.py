be84094c3019b20a837bc0520cda8489dbff443e
aysan-alizade <163893079+aysan-alizade@users.noreply.github.com>
2024-07-26 10:48:01 -0700

    add cemment for main

--- final.py
+++ final.py
@@ -4,7 +4,7 @@ def read_specific_lines(filename, line_numbers):
     with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
         selected_lines = [lines[i - 1].strip() for i in line_numbers]
-    return selected_lines
+    return 
 #this is a def to read q from file
 def uniq(x, y):
     file_path = 'all_Q.txt'
@@ -106,7 +106,7 @@ def result(numb):
     txt_b = "ویزگی ها "[::-1]
     if 0 <= numb < len(rlls):
         print(f"{txt_a}\n\n{rlls[numb]}\n{txt_b}\n\n{vi[numb]}")
-
+#the main def
 def main():
     while True:
         txt = "برای شرکت در ازمون کلیک کنید:"[::-1]
