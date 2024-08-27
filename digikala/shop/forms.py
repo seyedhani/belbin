addf4f223824f071ffc8f54a2fe980d5419011b9
aysan-alizade <163893079+aysan-alizade@users.noreply.github.com>
2024-08-27 10:04:48 -0700

    making form

--- digikala/shop/forms.py
+++ digikala/shop/forms.py
@@ -1,3 +1,31 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+ 
+class singupForms(UserCreationForm):
+    name = forms.CharField(
+        max_length=100 ,
+        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter your name'}))
+    
+    email = forms.EmailInput(
+        max_length=100 ,
+        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter your email'}))
+    
+    username = forms.CharField(
+        max_length=100 ,
+        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter your username'}))
+        
+    pass1 = forms.EmailInput(
+        max_length=100 ,
+        widget=forms.PasswordInput(
+            attrs={'class':'form-control','name':'password','type':'password','placeholder':'your pass'}))
+            
+    pass2 = forms.EmailInput(
+        max_length=100 ,
+        widget=forms.PasswordInput(
+            attrs={'class':'form-control','name':'password','type':'password','placeholder':'enter again'}))
+    class meta:
+        model =User
+        fields =('name','email','username' ,'pass1','pass2')
+    
+    
