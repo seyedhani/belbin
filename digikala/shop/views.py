a52b7084a4f8f89a8b76a04f758aebe7b8b62c1f
aysan-alizade <163893079+aysan-alizade@users.noreply.github.com>
2024-09-05 05:57:06 -0700

    add scoring

--- digikala/shop/views.py
+++ digikala/shop/views.py
@@ -43,7 +43,7 @@ def PQ(request , pk , sec):
     if request.method == "POST":
         form = ScoreFormForm(request.POST)
         if form.is_valid():
-            same = ScoreForm.objects.filter(main_q = main ,part_q = detail_records ,  user =request.user ).first()
+            same = ScoreForm.objects.filter(main_q = main ,part_q = detail_records ,  user =request.user ).first()      
             if same :
                 score_form = same
                 score_form.Score= form.cleaned_data.get('Score')
@@ -53,12 +53,41 @@ def PQ(request , pk , sec):
                 score_form.main_q = main
                 score_form.part_q = detail_records
             score_form.save()
+            
             records =  AggrQ.objects.all( )[sec::]   
+            if sec % 8 == 0:
+                q = 0
+                start = sec - 7
+                end = sec +1 
+                all_scored = True  # فرض بر این است که همه امتیاز داده شده‌اند
+                all = [ ]
+                for i in range(start, end):
+                    try:
+                        pq = AggrQ.objects.get(id=i)
+                    except AggrQ.DoesNotExist:
+                        continue  # اگر سوالی وجود نداشته باشد، ادامه می‌دهیم
+                    new = ScoreForm.objects.filter(main_q=main, part_q=pq, user=request.user).first()
+                    if new:
+                        all.append(new.Score )
+                    else:
+                        all_scored = False  # اگر به یکی از سوالات امتیاز داده نشده باشد، flag را False می‌کنیم
+                if all_scored:
+                    print("All questions in the range have been scored.")
+                    res = sum(all)
+                    if res > 10 :
+                        m = "قبل از ادامخ کلیک کنید و برخی پاسخ هارا ویرایش کنید تا امتیاز زیر ده باشد"
+                        messages.success(request, m , 'success')   
+                    
+                else:   
+                    print("Not all questions in the range have been scored.")
+                    
+                 
         return render(request , 'new.html' , {'records' : records} )
     else:
         form = ScoreFormForm()
     return render(request , 'qp.html' ,{'form' :form  })
 
+        
 def signupUser(request ):
     form = UserRegisterForm()
     if request.method == "POST":
